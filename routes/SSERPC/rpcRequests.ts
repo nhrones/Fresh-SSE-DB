17fd033f6ebebcca9d19103777b430727063001d
NDH <nick@ndhrones.com>
2023-12-08 07:58:46 -0500

    Update rpcRequests.ts

--- routes/SSERPC/rpcRequests.ts
+++ routes/SSERPC/rpcRequests.ts
@@ -1,13 +1,12 @@
 import { HandlerContext } from "$fresh/server.ts";
 
 const DEBUG = true
+const BC = new BroadcastChannel("sse-rpc");
 
 export const  handler = async (_req: Request, _ctx: HandlerContext): Promise<Response> => {
     const dataObject = await _req.json();
         if (DEBUG) console.info('Client Posted:', dataObject)
-        const BC = new BroadcastChannel("sse-rpc");
         BC.postMessage(dataObject);
-        BC.close();
         return new Response("",
         {
             status: 200,
