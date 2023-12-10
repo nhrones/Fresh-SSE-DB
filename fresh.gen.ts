6bbaa6c75575cd05b84ff54aad654ac49acf434a
NDH <nick@ndhrones.com>
2023-12-10 13:26:06 -0500

    renamed API SSERPC/rpcRequest.ts

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -3,8 +3,8 @@
 // This file is automatically updated during development when running `dev.ts`.
 
 import * as $SSERPC_kvRegistration from "./routes/SSERPC/kvRegistration.ts";
+import * as $SSERPC_kvRequest from "./routes/SSERPC/kvRequest.ts";
 import * as $SSERPC_remoteProcedures from "./routes/SSERPC/remoteProcedures.ts";
-import * as $SSERPC_rpcRequests from "./routes/SSERPC/rpcRequests.ts";
 import * as $_app from "./routes/_app.tsx";
 import * as $index from "./routes/index.tsx";
 
@@ -13,8 +13,8 @@ import { type Manifest } from "$fresh/server.ts";
 const manifest = {
   routes: {
     "./routes/SSERPC/kvRegistration.ts": $SSERPC_kvRegistration,
+    "./routes/SSERPC/kvRequest.ts": $SSERPC_kvRequest,
     "./routes/SSERPC/remoteProcedures.ts": $SSERPC_remoteProcedures,
-    "./routes/SSERPC/rpcRequests.ts": $SSERPC_rpcRequests,
     "./routes/_app.tsx": $_app,
     "./routes/index.tsx": $index,
   },
